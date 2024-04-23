36457c3392ef5ae6428083afb08ec737bdd98fbb
muzili <2586850402@qq.com>
2024-04-23 12:09:34 +0800

    改进 watchAD的调用方式
    仅传一个回调，在回调中判断状态

--- ADTest.ts
+++ ADTest.ts
@@ -7,10 +7,51 @@
 
 const {ccclass, property} = cc._decorator;
 
-interface AdEventParams {
+
+export class AdEvent{
+    private msg : string;
+    /**
+     * 仅在插全屏-激励视频视频有效, 广告是否完整的播放没有跳过
+     */
+    isSuccess : boolean = false;
+    /**
+     * 广告是否正在关闭
+     */
+    isClose : boolean = false;
+
+    /**
+     * 是否是播放广告出错,尝试使用 getMsg 获取错误内容
+     */
+    isError : boolean = false;
+    
+    AdEvent(msg = ""){
+        this.msg = msg;
+    }
+
+    getMsg() : string{
+        return this.msg;
+    }
+}
+
+/**
+ * 回调函数第一个参数的参数类型
+ */
+type ADEventCALL = (arg1: AdEvent) => void;
+
+/**
+ * 调用 watchAD添加事件列表的列表项内容
+ */
+export interface AdEventListParams {
     id : number;
-    success : Function;
-    error : Function;
+    event : ADEventCALL;
+}
+
+/**
+ * watchAD函数的第二个参数,播放广告时传入一些可选项，以JSON的方式
+ */
+interface ADwatchADExtra{
+    adType ?: string,
+    extra ?: string
 }
 
 //// 不包括开屏 开屏在java里面写
@@ -22,55 +63,62 @@ export enum ADType {
     ADDRAWFEED = "ADDRAWFEED"
 }
 
-export default class ADTest {
 
-    
+export default class ADTest {
     private static instance: ADTest = null;
     
-    private _Event: AdEventParams[] = [];
+    private _Event: AdEventListParams[] = [];
 
-    
+    private static registerEvent(){
+        // 注册监听
+        if (typeof window["callADLoadSuccess"] !== "function") {
+            window["callADLoadSuccess"] = function (id : number) {
+                for(let item of ADTest.getInstance()._Event){
+                    if(item.id == id){
+                        //item?.success();
+                    }
+                }
+            }
+        }
+        if (typeof window["callADLoadFail"] !== "function") {
+            window["callADLoadSuccess"] = function (id : number) {
+                for(let item of ADTest.getInstance()._Event){
+                    if(item.id == id){
+                        //item?.error();
+                    }
+                }
+            }
+        }
+        if (typeof window["callADShowSuccess"] !== "function") {
+            window["callADShowSuccess"] = function (id : number) {
+                for(let item of ADTest.getInstance()._Event){
+                    if(item.id == id){
+                        let requests : AdEvent = new AdEvent();
+                        requests.isSuccess = true;
+                        item?.event(requests);
+                        ADTest.instance.removeEvent(id);
+                    }
+                }
+            }
+        }
+        if (typeof window["callADShowFail"] !== "function") {
+            window["callADShowFail"] = function (id : number) { 
+                for(let item of ADTest.getInstance()._Event){
+                    if(item.id == id){
+                        let requests : AdEvent = new AdEvent();
+                        requests.isSuccess = false;
+                        item?.event(requests);
+                        ADTest.instance.removeEvent(id);
+                    }
+                }
+            }
+        }
+    }
+
     public static getInstance(): ADTest {
         if(ADTest.instance == null){
             ADTest.instance = new ADTest();
-            // 注册监听
-            if (typeof window["callADLoadSuccess"] !== "function") {
-                window["callADLoadSuccess"] = function (id : number) {
-                    for(let item of ADTest.getInstance()._Event){
-                        if(item.id == id){
-                            //item?.success();
-                        }
-                    }
-                }
-            }
-            if (typeof window["callADLoadFail"] !== "function") {
-                window["callADLoadSuccess"] = function (id : number) {
-                    for(let item of ADTest.getInstance()._Event){
-                        if(item.id == id){
-                            //item?.error();
-                        }
-                    }
-                }
-            }
-            if (typeof window["callADShowSuccess"] !== "function") {
-                window["callADShowSuccess"] = function (id : number) {
-                    for(let item of ADTest.getInstance()._Event){
-                        if(item.id == id){
-                            item?.success();
-                        }
-                    }
-                }
-            }
-            if (typeof window["callADShowFail"] !== "function") {
-                window["callADShowFail"] = function (id : number) { 
-                    for(let item of ADTest.getInstance()._Event){
-                        if(item.id == id){
-                            item?.error();
-                        }
-                    }
-                }
-            }
-
+            ADTest.registerEvent();
         }
         return ADTest.instance;
     }
@@ -81,18 +129,18 @@ export default class ADTest {
         jsb.reflection.callStaticMethod("ads/csjAdManager/AdJsManager", "LoadAdIO", "(Ljava/lang/String;ILjava/lang/String;)V", adId, num,adType);
     }
 
-    watchAD(event_succ: Function, event_err : Function = null, adType : ADType = ADType.ADFULLSCREEN) : number {
+    watchAD(event : ADEventCALL,extra : ADwatchADExtra = {}) : number{
         let _id : number = (new Date()).getMilliseconds();
         this._Event.push({
             id : _id,
-            success: event_succ,
-            error: event_err
+            event: event
         });
-        // TODO: 然后发送播放广告, 把id带过去
-        jsb.reflection.callStaticMethod("ads/csjAdManager/AdJsManager", "ShowAdIO", "(Ljava/lang/String;I)V", adType, _id);
+        if(!extra.adType) extra.adType = ADType.ADFULLSCREEN;
+        if(!extra.extra) extra.extra = "{}"
+        jsb.reflection.callStaticMethod("ads/csjAdManager/AdJsManager", "ShowAdIO", "(Ljava/lang/String;ILjava/lang/String;)V", extra.adType, _id,extra.extra);
         return _id;
     }
-
+    
     removeEvent(id : number){
         for (var i = 0; i < this._Event.length; i++) {
             if (this._Event[i].id == id) {
